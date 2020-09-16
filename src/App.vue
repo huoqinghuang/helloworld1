42bdcc6665c8d0e3c768b969bcc8851e4f816194
霍庆煌 <1539766176@qq.com>
2020-09-16 23:23:31 +0800

    第三次提交

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <h2>预祝大家 都能知道自己心仪的 offer</h2>
-    <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2244407442,3539065501&fm=26&gp=0.jpg"/>
-    <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600233697591&di=0975e6c8f4f41586bb6451674d13ed77&imgtype=0&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D2681546830%2C1272261180%26fm%3D214%26gp%3D0.jpg"/>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600277199083&di=1d9a53302d4ec57c27d8c3821b9e3295&imgtype=0&src=http%3A%2F%2Fa2.att.hudong.com%2F36%2F48%2F19300001357258133412489354717.jpg"/>
   </div>
 </template>
